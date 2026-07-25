cc0b519aaacd744a4c69b3fa6b1365eb328a5045
abz <teshalea000@gmail.com>
2026-07-25 22:24:10 +0530

    configured for deployment

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-const port = 5500;
+const port = process.env.PORT || 5500;
 
 // Middleware to parse JSON and URL-encoded data
 app.use(express.json());
